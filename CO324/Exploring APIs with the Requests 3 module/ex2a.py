f015d043bd4c9033495a78c22861a656efa9c7eb
praveen <praveenbhananjaya@gmail.com>
2020-08-16 22:11:24 +0530

    test1

--- CO324/Exploring APIs with the Requests 3 module/ex2a.py
+++ CO324/Exploring APIs with the Requests 3 module/ex2a.py
@@ -15,27 +15,26 @@ from typing import List, Tuple
 def takeSecond(elem):
     return elem[0]
 
-def github_superstars(orgnanization: str) -> List[Tuple]:
+def github_superstars(orgnanization: str) -> List[Tuple]:   #
 
-    orgnanization = orgnanization.replace("https://github.com/","")
-  #  print(orgnanization)
-    Out = []
+    Out = []        #out
     #Sirasa superstar for Computer engineers. :
     with requests.Session() as session:
         session.headers['Authorization'] = 'token d5ceb65bbc67f1a4f92709d31d4d5c2171daef9c'
-
-        # members
+        # token
+        # members list
         git_memebrs = session.get(f'https://api.github.com/orgs/{orgnanization}/members')
 
-
         for name in git_memebrs.json():        # list of members
-
             git_memebrs_repo = session.get(f"https://api.github.com/users/{name['login']}/repos")   # member repos
 
             # read data
             repo_name = ""
             repo_star = 0
 
+            #  For each member, find the repo they own with the most stars.
+
+            #  Add the repo name and the number of stars it has to a list.
             for star in git_memebrs_repo.json() :
                 #print(star['name'] ,star['stargazers_count'])
                 if( repo_name == "" ):
@@ -54,5 +53,10 @@ def github_superstars(orgnanization: str) -> List[Tuple]:
         return Out
 
 
-print( github_superstars("https://github.com/cepdnaclk") )
+#part b
+orgnanization = "https://github.com/cepdnaclk"  # orgnanization
+
+orgnanization = orgnanization.replace("https://github.com/","")    #extrack orgnanization
+
+print( github_superstars( orgnanization ))  # print most star
 
