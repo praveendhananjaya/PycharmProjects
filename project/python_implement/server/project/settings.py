60bd066ee86e08d0b8205d709bd171a3bff3f871
praveen <praveenbhananjaya@gmail.com>
2021-01-15 20:04:36 +0530

    cv c

--- project/python_implement/server/project/settings.py
+++ project/python_implement/server/project/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(os.path.dirname(BASE_DIR), 'static'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
